bb7fe3b1ebd6edc2146d4175698db6a4de534762
unknown <1540274138@qq.com>
2016-08-14 17:26:50 -0600

    changes log

--- CG_bl/WebContent/js/index.js
+++ CG_bl/WebContent/js/index.js
@@ -1,5 +1,5 @@
 
-console.log("windows11")
+//console.log("windows11")
 
 var imgSize = [
 
@@ -7,13 +7,10 @@ var imgSize = [
                {width:1719,height:732},
                {width:935,height:971},
                {width:682,height:402},
-             
-               
                {width:994,height:357}
-               
                ];
 
-console.log(imgSize[0]);
+//console.log(imgSize[0]);
 
 $.ajax({
 		type:"POST",
@@ -21,14 +18,14 @@ $.ajax({
 		url:"getJson.action",
 		async: false
 	}).done(function(data){
-		console.log(data)
+		//console.log(data)
 		var html="";
 		var row = data.rows;
 			$.each(row,function(i,index){
 				var li = "<li class="+index.id+"></li>"
 				html+=li;
 			})
-		console.log(html)
+		//console.log(html)
 		$(".boald .wall").html(html);
 	})
   var mark = false;
@@ -37,7 +34,7 @@ $.ajax({
   createImg()
   var index;
  function createImg(){
-	 
+
 	  $(".wall li").click(function(){
 		 var _this = $(this);
 		  index = $(this).index();
@@ -48,73 +45,67 @@ $.ajax({
 			  async: false
 		  }).done(function(data){
 			   var html="";
-			  	console.log(data);
+			  	//console.log(data);
 			  	var row = data.rows;
-			  	console.log(row)
-			  	console.log(index)
+			  	//console.log(row)
+			  	//console.log(index)
 			  	var dataImg = data.rows[0];
-			  	console.log(dataImg);
-			  	console.log(dataImg.name)
+			  	//console.log(dataImg);
+			  	//console.log(dataImg.name)
 			  	var li = "<img src="+dataImg.name+">"
 			  	_this.html(li)
-			  
+
 			  	init(_this)
 		  	})
-		  
+
   });
 
  }
-  
 
-  
+
+
 function init(_this){
-  
-    
-   
-    
+
+
+
+
     var screenImage = _this.find('img');
-   
+
     setTimeout(function(){
-    	
-    	
-    	
     	var imgSrc = screenImage.attr("src");
         var theImage = $("<img/>");
 
         theImage.attr({"id":"ImgID","src":imgSrc})
           var theImage2 = new Image();
-         
+
           theImage2.src = imgSrc;
 
           var imageWidth = theImage2.width;
           var imageHeight = theImage2.height;
-          console.log(imageHeight)
+          //console.log(imageHeight)
 
           var div = $('<div class="model"><div class="box"><img id="loading" src=img/fz.gif></div></div>');
-          console.log($("#loading"))
-         
+          //console.log($("#loading"))
+
           div.append(theImage);
-         
+
           $('body').append(div);
-         
+
           $(".box").bind("click", function(e) {
-           
-        	  
+
+
                 if (e.currentTarget.className=="model") {
                  // $(".model").remove();
 
                 }
            });
-         
-         
+
+
           changeSize0(imageWidth,imageHeight,div,theImage,$("#ImgID"))
 
-    	
+
     },500)
 }
-
-
-
 
 
 
@@ -123,74 +114,123 @@ function changeSize0(width,height,div,theImage,
 		id
 		) {
 	// debugger;
-	console.log("box")
-  // console.log(id);
+	//console.log("box")
+  // //console.log(id);
   var oWith=400;
   var oheight =0;
   var opa = 0.3;
-  
+
   var time = setInterval(function(){
-	  console.log("time")
+	 // //console.log("time")
       var flag = false;
         var widthFlag;
     // oWith = oWith+5;
       oheight = oheight+5;
-      // console.log(oheight);
-     // console.log(height);
+      // //console.log(oheight);
+     // //console.log(height);
       var percent = width/height;
       if(oheight>=height+40){
         oheight = height+40;
         clearInterval(time)
         flag = true;
-        // console.log(flag);
+        // //console.log(flag);
         if (flag) {
-        	console.log("flag")
+        	////console.log("flag")
           changeWidth(oWith,width,id)
           $(".box").append(theImage);
-          // console.log(widthFlag);
+
+          // //console.log(widthFlag);
         }
 
       }
-      	console.log("oWith"+oWith)
-       $(".box").css({"width":oWith,"height":oheight,"left":'50%',"marginLeft":-oWith/2,"top":'50%',"marginTop":-oheight/2})
-     
+      //	//console.log("oWith"+oWith)
+       $(".box").css({"height":oheight,"left":'50%',"top":'50%',"marginTop":-oheight/2})
+
 
   },1)
 
 }
-  
+
 function changeWidth(oWith,width,id){
+    var changeOpFlag = true;
+
+
 	  var time2 = setInterval(function(){
-		  oWith+=5;
-		  if(oWith>=width+40){
-			  	clearInterval(time2)
-		  	}
-		  
+
+		  if(oWith<width+40){
+
+			  oWith=oWith+5;
+			  ////console.log(oWith+":"+width)
+			 // //console.log($.type(width))
+			  if(oWith>=(width+40)){
+
+					clearInterval(time2)
+			        changeOpFlag = false;
+		          if(changeOpFlag==false){
+
+		            changImgOpacity(id);
+
+		          }
+			 }
+
+		  }else{
+
+			  oWith-=5;
+			  if(oWith<=width+40){
+						  	clearInterval(time2)
+			          changeOpFlag = false;
+			          if(changeOpFlag==false){
+
+			            changImgOpacity(id)
+			          }
+			 }
+
+
+		  }
+
+
 		  $(".box").css({"width":oWith,"left":'50%',"marginLeft":-oWith/2,"top":'50%'})
 		  },1)
-		 
+
   }
-	  
-	  
+
+
 
 
 function eventBindPre(){
-  
 
-// console.log("markæ§è¡");
+
+
  var turnDiv = $("<div class='turnPre turn'>PREV</div>");
- $("body").find(".model").find(".box").append(turnDiv)
+ var turnDivNExt = $("<div class='turnNext turn'>PREV</div>");
+
+ //debugger;
+ if(index>0){
+
+	 $("body").find(".model").find(".box").append(turnDiv)
+   $(".turnPre").css({"display":"none"})
+ }else{
+	 //debugger;
+	// $("body").find(".model").find(".box").find(".turnPre").css({"display":"none"});
+	 $("body").find(".model").find(".box").find(".turnPre").remove();
+	 //console.log($("body").find(".model").find(".box").find(".turnPre"))
+
+
+ }
+ ////console.log($(".turnPre"))
 
  $(".turnPre").bind("click",function(){
-	 
+  $(".turn").remove();
 	 $("body").find(".model").find(".box").find("img").css({opacity:0});
-	 debugger;
+
    index--;
-   console.log("--"+index)
+   //console.log("--"+index)
     if (index<0) {
       index=4;
+      //debugger;
+
     }
-    
+
     $.ajax({
 		  type:"POST",
 		  data:{index:index},
@@ -198,39 +238,39 @@ function eventBindPre(){
 		  async: false
 	  }).done(function(data){
 		   var html="";
-		  	// console.log(data);
+		  	// //console.log(data);
 		  	var row = data.rows;
 			var dataImg = data.rows[0];
-		  	console.log(dataImg.name)
-		  
+		  	//console.log(dataImg.name)
+
 		  	var li = "<img src="+dataImg.name+">";
 		  	$(".wall li").eq(index).html(li)
 		  	// debugger;
-		  	 console.log($(".wall li").eq(index));
+		  	 //console.log($(".wall li").eq(index));
 		  		init2(dataImg.name)
-		  
+
 	  	})
         // $(".model").remove();
 	  	// debugger;
-       // console.log("addindx:"+index);
+       // //console.log("addindx:"+index);
        function init2(dataImg){
     	  var theImage = $("img");
            setTimeout(function(){
         	  // debugger;
         	   var imgUp = $(".wall li").eq(index).find("img")
         	   var imgSrc = dataImg;
-        	   console.log(dataImg)
-             
+        	   //console.log(dataImg)
+
         	   theImage.attr("src",dataImg);
-        	   console.log(theImage)
+        	   //console.log(theImage)
                // theImage.src = imgSrc;
                  var theImage2 = new Image();
-                 // console.log(theImage2);
+                 // //console.log(theImage2);
                  theImage2.src = dataImg;
 
                  var imageWidth = theImage2.width;
                  var imageHeight = theImage2.height;
-               // console.log(imageWidth);
+               // //console.log(imageWidth);
 
                 // var div = $(".model")
                 // var div2 = $('<div class="box"><img id="loading"
@@ -240,9 +280,9 @@ function eventBindPre(){
                 // div.append(theImage);
                 // debugger;
                 /*
-				 * $(".box").bind("click", function(e) { //console.log(2); //
-				 * console.log(e.currentTarget.className);
-				 * 
+				 * $(".box").bind("click", function(e) { ////console.log(2); //
+				 * //console.log(e.currentTarget.className);
+				 *
 				 * if (e.currentTarget.className="model") { //
 				 * $(".model").remove();
 				 *  } });
@@ -251,74 +291,75 @@ function eventBindPre(){
                  var oheightSize = imgSize[index].height;
                  var oWithSize2 = imgSize[index+1].width;
                  var oheightSize2 = imgSize[index+1].height;
-                 // debugger;
+               //  debugger;
                  changeSize(oWithSize2,oheightSize2,$("#ImgID"),oWithSize,oheightSize)
-        	   
-        	   
+
+
            },1000)
-    	   
+
        }
  })
 
 }
 
 function changeSize(width,height,id,oWithSize,oheightSize) {
-	
-	console.log("box")
-  // console.log(id);
+
+	console.log("next")
+  // //console.log(id);
   var oWith=width;
   var oheight = height;
   var opa = 0.3;
-  
+
   var time = setInterval(function(){
 
       var flag = false;
         var widthFlag;
     // oWith = oWith+5;
-     
-      // console.log(oheight);
-     // console.log(height);
+
+      // //console.log(oheight);
+     // //console.log(height);
       var percent = width/height;
-      
+
       if(oheight<oheightSize){
     	  oheight = oheight+3;
     	  if(oheight>=oheightSize+40){
     	        oheight = oheightSize+40;
     	        clearInterval(time)
     	        flag = true;
-    	        // console.log(flag);
+    	        // //console.log(flag);
     	        if (flag) {
     	          changeWidth(oWith,oWithSize,id)
     	          // $(".box").append(theImage);
-    	          // console.log(widthFlag);
+    	          // //console.log(widthFlag);
     	        }
 
     	      }
-    	  
+
       }else{
-    	  
+
     	  oheight = oheight-3;
     	  if(oheight<=oheightSize+40){
     	        oheight = oheightSize+40;
     	        clearInterval(time)
     	        flag = true;
-    	        // console.log(flag);
+    	        // //console.log(flag);
     	        if (flag) {
     	          changeWidth(oWith,oWithSize,id)
     	          // $(".box").append(theImage);
-    	          // console.log(widthFlag);
+    	          // //console.log(widthFlag);
+
     	        }
 
     	      }
-    	  
-      }
-    
-   
-      $(".box").css({"width":oWith,"height":oheight,"left":'50%',"marginLeft":-oWith/2,"top":'50%',"marginTop":-oheight/2})
-     
 
+      }
+
+
+      $(".box").css({"width":oWith,"height":oheight,"left":'50%',"marginLeft":-oWith/2,"top":'50%',"marginTop":-oheight/2})
+
+      //console.log($(".turn").css({"display":"block"}));
   },3)
-  
+
 }
 
 
@@ -331,32 +372,83 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
 	  var opa = 0;
 	  var opaTime = setInterval(function(){
 	        opa =  opa+0.01;
-	      // console.log(opa);
-	     // console.log(id);
+	      // //console.log(opa);
+	     // //console.log(id);
 	        id.css({"opacity":opa})
 	        if (opa>=1) {
 	          clearInterval(opaTime);
 	          mark = true;
+            $("#loading").remove();
 	          if (mark==true) {
-	                // eventBindNext();
+	                eventBindNext();
 	                eventBindPre();
+                  console.log(id.width());
+                  var ImgWidth =id.width()/2;
+                //  debugger;
+                //  console.log($(".box"));
+                  $(".box").on("mouseover",function(e){
+
+                        var  x=$(this).position().left;
+                          console.log(x);
+                          $(this).on("mousemove",function(e){
+                            var  x = $(this).position().left;
+                            var clientX = e.clientX;
+                            var offsetX = $(this).offset().left;
+                          //  console.log(x+":"+clientX+":"+offsetX);
+                          var left = (clientX - offsetX);
+                        //  console.log("left"+left);
+                              if (left>=ImgWidth) {
+                                //  debugger;
+                                  $(".turnNext").css({"display":"block"})
+                                  $(".turnPre").css({"display":"none"})
+                                  $('.turn').css({"color":"#fff"});
+
+                              }else{
+                                  $(".turnPre").css({"display":"block"})
+                                      $(".turnNext").css({"display":"none"})
+
+                              }
+                          })
+
+                  })
 	          }
+
+
 	        }
 	  },1)
 	}
 
  function eventBindNext(){
-    var turnDiv = $("<div class='turnNext turn'>NEXT</div>");
-    $("body").find(".model").find(".box").append(turnDiv)
+	 //console.log("eventBindNext");
+  // debugger;
+   console.log(index);
+   $(".turn").remove();
+       var turnDiv = $("<div class='turnNext turn'>NEXT</div>");
+    console.log($(".turn"));
+    if (index<=4) {
 
+       $("body").find(".model").find(".box").append(turnDiv)
+        $(".turnNext").css({"display":"none"})
+    }
+
+    if (index>4) {
+      index=0;
+    }
+    if (index>=4) {
+       console.log($("body").find(".model").find(".box").find(".turnNext"));
+         $("body").find(".model").find(".box").find(".turnNext").remove();
+    }
     $(".turnNext").bind("click",function(){
+      console.log($(".turnNext"));
+      $(".turnNext").css({"display":"none"});
+      $("turn").css({"display":"block"})
+      debugger;
+      $(".turnNext turn").css({"display":"none"})
+      $("body").find(".model").find(".box").find("img").css({opacity:0});
+      //debugger;
        index++
 
-       if (index>=5) {
-         index=0;
-       }
-      
-        
+
         $.ajax({
     		  type:"POST",
     		  data:{index:index},
@@ -364,50 +456,63 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
     		  async: false
     	  }).done(function(data){
     		   var html="";
-    		  	// console.log(data);
+    		  	// //console.log(data);
     		  	var row = data.rows;
     			var dataImg = data.rows[0];
-    		  	console.log(dataImg.name)
-    		  
+    		  	//console.log(dataImg.name)
+
     		  	var li = "<img src="+dataImg.name+">";
     		  	$(".wall li").eq(index).html(li)
-    		  	 console.log($(".wall li").eq(index));
-    		  		init()
-    		  	
+    		  	 //console.log($(".wall li").eq(index));
+    		  		init3(dataImg.name)
+
     	  	})
-            $(".model").remove();
-           console.log("addindx:"+index);
-           function init(){
-        	   console.log($(".wall li").eq(index));
-               setTimeout(function(){
-            	   var imgSrc = $(".wall li").eq(index).find("img").attr("src");
-            	   console.log(imgSrc)
-                   var theImage = $("<img/>");
+          //  $(".model").remove();
+           //console.log("addindx:"+index);
+           function init3(dataImg){
 
-                   theImage.attr({"id":"ImgID","src":imgSrc})
-                     var theImage2 = new Image();
-                     // console.log(theImage2);
-                     theImage2.src = imgSrc;
 
-                     var imageWidth = theImage2.width;
-                     var imageHeight = theImage2.height;
-                   // console.log(imageWidth);
 
-                     var div = $('<div class="model"><div class="box"><img id="loading" src=img/fz.gif></div></div>');
-                     $("#ImgID").remove();
-                     div.append(theImage);
 
-                     $('body').append(div);
-                     /*
-						 * $(".box").bind("click", function(e) {
-						 * //console.log(2); //
-						 * console.log(e.currentTarget.className);
-						 * 
-						 * if (e.currentTarget.className=="model") {
-						 * $(".model").remove(); } });
-						 */
-                     changeSize(imageWidth,imageHeight,div,theImage,$("#ImgID"))
-               },500)   
-           }   
+
+         	  var theImage = $("img");
+                setTimeout(function(){
+             	  // debugger;
+             	   var imgUp = $(".wall li").eq(index).find("img")
+             	   var imgSrc = dataImg;
+             	   console.log(dataImg)
+
+             	   theImage.attr("src",dataImg);
+             	   //console.log(theImage)
+                    // theImage.src = imgSrc;
+                      var theImage2 = new Image();
+                      // //console.log(theImage2);
+                      theImage2.src = dataImg;
+
+                      var imageWidth = theImage2.width;
+                      var imageHeight = theImage2.height;
+
+                      var oWithSize = imgSize[index].width;
+                      var oheightSize = imgSize[index].height;
+                      var oWithSize2 = imgSize[index-1].width;
+                      var oheightSize2 = imgSize[index-1].height;
+                    //  debugger;
+                      changeSize(oWithSize2,oheightSize2,$("#ImgID"),oWithSize,oheightSize)
+
+
+                },1000)
+
+            }
+
      })
 }
+
+
+$(".box").on("mousemove",function(e){
+    console.log("mousemove");
+      var  x=$(this).position();
+        console.log(x);
+
+})
+
+console.log(mark);
