dd7ca3ce1238ded89d8e011c317e0417d0e1c56b
qiudaeoermu <1540274138@qq.com>
2016-08-14 22:32:51 -0600

    1.0版本

--- CG_bl/WebContent/js/index.js
+++ CG_bl/WebContent/js/index.js
@@ -1,17 +1,11 @@
 
-//console.log("windows11")
-
 var imgSize = [
-
                {width:600,height:790},
                {width:1719,height:732},
                {width:935,height:971},
                {width:682,height:402},
                {width:994,height:357}
                ];
-
-//console.log(imgSize[0]);
-
 $.ajax({
 		type:"POST",
 		data:{},
@@ -66,11 +60,7 @@ $.ajax({
 
 function init(_this){
 
-
-
-
     var screenImage = _this.find('img');
-
     setTimeout(function(){
     	var imgSrc = screenImage.attr("src");
         var theImage = $("<img/>");
@@ -101,21 +91,18 @@ function init(_this){
            });
 
 
-          changeSize0(imageWidth,imageHeight,div,theImage,$("#ImgID"))
+          changeSizeOrigal(imageWidth,imageHeight,div,theImage,$("#ImgID"))
 
 
     },500)
 }
 
 
+/*
+第一次点击出现图片
+*/
 
-
-function changeSize0(width,height,div,theImage,
-		id
-		) {
-	// debugger;
-	//console.log("box")
-  // //console.log(id);
+function changeSizeOrigal(width,height,div,theImage,id) {
   var oWith=400;
   var oheight =0;
   var opa = 0.3;
@@ -151,156 +138,10 @@ function changeSize0(width,height,div,theImage,
 
 }
 
-function changeWidth(oWith,width,id){
-    var changeOpFlag = true;
 
-
-	  var time2 = setInterval(function(){
-
-		  if(oWith<width+40){
-
-			  oWith=oWith+5;
-			  ////console.log(oWith+":"+width)
-			 // //console.log($.type(width))
-			  if(oWith>=(width+40)){
-
-					clearInterval(time2)
-			        changeOpFlag = false;
-		          if(changeOpFlag==false){
-
-		            changImgOpacity(id);
-
-		          }
-			 }
-
-		  }else{
-
-			  oWith-=5;
-			  if(oWith<=width+40){
-						  	clearInterval(time2)
-			          changeOpFlag = false;
-			          if(changeOpFlag==false){
-
-			            changImgOpacity(id)
-			          }
-			 }
-
-
-		  }
-
-
-		  $(".box").css({"width":oWith,"left":'50%',"marginLeft":-oWith/2,"top":'50%'})
-		  },1)
-
-  }
-
-
-
-
-function eventBindPre(){
-
-
-
- var turnDiv = $("<div class='turnPre turn'>PREV</div>");
- var turnDivNExt = $("<div class='turnNext turn'>PREV</div>");
-
- //debugger;
- if(index>0){
-
-	 $("body").find(".model").find(".box").append(turnDiv)
-   $(".turnPre").css({"display":"none"})
- }else{
-	 //debugger;
-	// $("body").find(".model").find(".box").find(".turnPre").css({"display":"none"});
-	 $("body").find(".model").find(".box").find(".turnPre").remove();
-	 //console.log($("body").find(".model").find(".box").find(".turnPre"))
-
-
- }
- ////console.log($(".turnPre"))
-
- $(".turnPre").bind("click",function(){
-  $(".turn").remove();
-	 $("body").find(".model").find(".box").find("img").css({opacity:0});
-
-   index--;
-   //console.log("--"+index)
-    if (index<0) {
-      index=4;
-      //debugger;
-
-    }
-
-    $.ajax({
-		  type:"POST",
-		  data:{index:index},
-		  url:"getImg.action",
-		  async: false
-	  }).done(function(data){
-		   var html="";
-		  	// //console.log(data);
-		  	var row = data.rows;
-			var dataImg = data.rows[0];
-		  	//console.log(dataImg.name)
-
-		  	var li = "<img src="+dataImg.name+">";
-		  	$(".wall li").eq(index).html(li)
-		  	// debugger;
-		  	 //console.log($(".wall li").eq(index));
-		  		init2(dataImg.name)
-
-	  	})
-        // $(".model").remove();
-	  	// debugger;
-       // //console.log("addindx:"+index);
-       function init2(dataImg){
-    	  var theImage = $("img");
-           setTimeout(function(){
-        	  // debugger;
-        	   var imgUp = $(".wall li").eq(index).find("img")
-        	   var imgSrc = dataImg;
-        	   //console.log(dataImg)
-
-        	   theImage.attr("src",dataImg);
-        	   //console.log(theImage)
-               // theImage.src = imgSrc;
-                 var theImage2 = new Image();
-                 // //console.log(theImage2);
-                 theImage2.src = dataImg;
-
-                 var imageWidth = theImage2.width;
-                 var imageHeight = theImage2.height;
-               // //console.log(imageWidth);
-
-                // var div = $(".model")
-                // var div2 = $('<div class="box"><img id="loading"
-				// src=img/fz.gif></div>');
-                // div.append(div2)
-                // $("#ImgID").remove();
-                // div.append(theImage);
-                // debugger;
-                /*
-				 * $(".box").bind("click", function(e) { ////console.log(2); //
-				 * //console.log(e.currentTarget.className);
-				 *
-				 * if (e.currentTarget.className="model") { //
-				 * $(".model").remove();
-				 *  } });
-				 */
-                 var oWithSize = imgSize[index].width;
-                 var oheightSize = imgSize[index].height;
-                 var oWithSize2 = imgSize[index+1].width;
-                 var oheightSize2 = imgSize[index+1].height;
-               //  debugger;
-                 changeSize(oWithSize2,oheightSize2,$("#ImgID"),oWithSize,oheightSize)
-
-
-           },1000)
-
-       }
- })
-
-}
+/*
+  点击左右按钮执行的change
+*/
 
 function changeSize(width,height,id,oWithSize,oheightSize) {
 
@@ -347,24 +188,49 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
     	          changeWidth(oWith,oWithSize,id)
     	          // $(".box").append(theImage);
     	          // //console.log(widthFlag);
-
     	        }
-
     	      }
-
       }
-
-
       $(".box").css({"width":oWith,"height":oheight,"left":'50%',"marginLeft":-oWith/2,"top":'50%',"marginTop":-oheight/2})
 
       //console.log($(".turn").css({"display":"block"}));
-  },3)
+  },1)
 
 }
 
 
 
+function changeWidth(oWith,width,id){
+    var changeOpFlag = true;
+	  var time2 = setInterval(function(){
 
+		  if(oWith<width+40){
+
+			  oWith=oWith+5;
+			  ////console.log(oWith+":"+width)
+			 // //console.log($.type(width))
+			  if(oWith>=(width+40)){
+
+					clearInterval(time2)
+			        changeOpFlag = false;
+		          if(changeOpFlag==false){
+		            changImgOpacity(id);
+		          }
+			 }
+		  }else{
+			  oWith-=5;
+			  if(oWith<=width+40){
+						  	clearInterval(time2)
+			          changeOpFlag = false;
+			          if(changeOpFlag==false){
+
+			            changImgOpacity(id)
+			          }
+			 }
+		  }
+		  $(".box").css({"width":oWith,"left":'50%',"marginLeft":-oWith/2,"top":'50%'})
+		  },1)
+  }
 
 
 	function changImgOpacity(id){
@@ -380,37 +246,44 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
 	          mark = true;
             $("#loading").remove();
 	          if (mark==true) {
+	        	  debugger;
+	              $(".box").on("mouseover",function(e){
+
+		              var  x = $(this).position().left;
+		                console.log(x);
+		                $(this).on("mousemove",function(e){
+		                  var  x = $(this).position().left;
+		                  var clientX = e.clientX;
+		                  var offsetX = $(this).offset().left;
+		                //  console.log(x+":"+clientX+":"+offsetX);
+		                var left = (clientX - offsetX);
+		              //  console.log("left"+left);
+		                console.log(id.width());
+		                var ImgWidth =id.width()/2;
+
+		                    if (left>=ImgWidth) {
+		                      //  debugger;
+		                        $(".turnNext").css({"display":"block"})
+		                        $(".turnPre").css({"display":"none"})
+		                    }else{
+		                        $(".turnPre").css({"display":"block"})
+		                        $(".turnNext").css({"display":"none"})
+
+		                    }
+		                })
+
+	              	})
+	        	  //debugger;
 	                eventBindNext();
 	                eventBindPre();
-                  console.log(id.width());
-                  var ImgWidth =id.width()/2;
+
                 //  debugger;
                 //  console.log($(".box"));
-                  $(".box").on("mouseover",function(e){
 
-                        var  x=$(this).position().left;
-                          console.log(x);
-                          $(this).on("mousemove",function(e){
-                            var  x = $(this).position().left;
-                            var clientX = e.clientX;
-                            var offsetX = $(this).offset().left;
-                          //  console.log(x+":"+clientX+":"+offsetX);
-                          var left = (clientX - offsetX);
-                        //  console.log("left"+left);
-                              if (left>=ImgWidth) {
-                                //  debugger;
-                                  $(".turnNext").css({"display":"block"})
-                                  $(".turnPre").css({"display":"none"})
-                                  $('.turn').css({"color":"#fff"});
 
-                              }else{
-                                  $(".turnPre").css({"display":"block"})
-                                      $(".turnNext").css({"display":"none"})
+                  debugger;
+                  mark = false;
 
-                              }
-                          })
-
-                  })
 	          }
 
 
@@ -418,10 +291,100 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
 	  },1)
 	}
 
+  /*
+  向左点击
+  */
+
+  function eventBindPre(){
+   var turnDiv = $("<div class='turnPre turn'>PREV</div>");
+   var turnDivNExt = $("<div class='turnNext turn'>PREV</div>");
+
+   //debugger;
+   if(index>0){
+
+  	 $("body").find(".model").find(".box").append(turnDiv)
+     $(".turnPre").css({"display":"none"})
+   }else{
+  	 //debugger;
+  	// $("body").find(".model").find(".box").find(".turnPre").css({"display":"none"});
+  	 $("body").find(".model").find(".box").find(".turnPre").remove();
+  	 //console.log($("body").find(".model").find(".box").find(".turnPre"))
+
+
+   }
+   ////console.log($(".turnPre"))
+
+   $(".turnPre").bind("click",function(){
+    $(".turn").remove();
+    $("body").find(".model").find(".box").find("img").css({opacity:0});
+
+     index--;
+     //console.log("--"+index)
+      if (index<0) {
+        index=4;
+        //debugger;
+
+      }
+
+      $.ajax({
+  		  type:"POST",
+  		  data:{index:index},
+  		  url:"getImg.action",
+  		  async: false
+  	  }).done(function(data){
+  		   var html="";
+  		  	// //console.log(data);
+  		  	var row = data.rows;
+  			var dataImg = data.rows[0];
+  		  	//console.log(dataImg.name)
+
+  		  	var li = "<img src="+dataImg.name+">";
+  		  	$(".wall li").eq(index).html(li)
+  		  	// debugger;
+  		  	 //console.log($(".wall li").eq(index));
+  		  		init(dataImg.name)
+
+  	  	})
+          // $(".model").remove();
+  	  	// debugger;
+         // //console.log("addindx:"+index);
+         function init(dataImg){
+      	  var theImage = $("img");
+             setTimeout(function(){
+          	  // debugger;
+          	   var imgUp = $(".wall li").eq(index).find("img")
+          	   var imgSrc = dataImg;
+          	   //console.log(dataImg)
+
+          	   theImage.attr("src",dataImg);
+          	   //console.log(theImage)
+                 // theImage.src = imgSrc;
+                   var theImage2 = new Image();
+                   // //console.log(theImage2);
+                   theImage2.src = dataImg;
+
+                   var imageWidth = theImage2.width;
+                   var imageHeight = theImage2.height;
+
+                   var oWithSize = imgSize[index].width;
+                   var oheightSize = imgSize[index].height;
+                   var oWithSize2 = imgSize[index+1].width;
+                   var oheightSize2 = imgSize[index+1].height;
+                 //  debugger;
+                   changeSize(oWithSize2,oheightSize2,$("#ImgID"),oWithSize,oheightSize)
+             },1000)
+
+         }
+   })
+
+  }
+/*
+向右点击
+*/
+
  function eventBindNext(){
-	 //console.log("eventBindNext");
-  // debugger;
-   console.log(index);
+
+
    $(".turn").remove();
        var turnDiv = $("<div class='turnNext turn'>NEXT</div>");
     console.log($(".turn"));
@@ -439,10 +402,12 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
          $("body").find(".model").find(".box").find(".turnNext").remove();
     }
     $(".turnNext").bind("click",function(){
+    	$(".turn").remove();
+   	   // $("body").find(".model").find(".box").find("img").css({opacity:0});
       console.log($(".turnNext"));
       $(".turnNext").css({"display":"none"});
-      $("turn").css({"display":"block"})
-      debugger;
+
+      //debugger;
       $(".turnNext turn").css({"display":"none"})
       $("body").find(".model").find(".box").find("img").css({opacity:0});
       //debugger;
@@ -464,17 +429,12 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
     		  	var li = "<img src="+dataImg.name+">";
     		  	$(".wall li").eq(index).html(li)
     		  	 //console.log($(".wall li").eq(index));
-    		  		init3(dataImg.name)
+    		  		init(dataImg.name)
 
     	  	})
           //  $(".model").remove();
            //console.log("addindx:"+index);
-           function init3(dataImg){
-
-
-
-
-
+           function init(dataImg){
          	  var theImage = $("img");
                 setTimeout(function(){
              	  // debugger;
@@ -498,21 +458,8 @@ function changeSize(width,height,id,oWithSize,oheightSize) {
                       var oheightSize2 = imgSize[index-1].height;
                     //  debugger;
                       changeSize(oWithSize2,oheightSize2,$("#ImgID"),oWithSize,oheightSize)
-
-
                 },1000)
 
             }
-
      })
 }
-
-
-$(".box").on("mousemove",function(e){
-    console.log("mousemove");
-      var  x=$(this).position();
-        console.log(x);
-
-})
-
-console.log(mark);
